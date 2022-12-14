2a88d7b2d8b663b4b4767457e6b0677132a76596
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-13 18:06:20 -0800

    Mock two posts and render with map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 export default function App()
 {
+    const posts = [
+        {
+            id: 1,
+            text: 'Hello, world!',
+            timestamp: 'a minute ago',
+            author: {
+                username: 'susan',
+            },
+        },
+        {
+            id: 2,
+            text: 'Second post',
+            timestamp: 'an hour ago',
+            author: {
+                username: 'john',
+            },
+        },
+    ];
+
     return (
-        <h1>Microblog</h1>
+        <>
+            <h1>Microblog</h1>
+            {posts.map(post => {
+                return (
+                    <p>
+                        <b>
+                            {post.author.username}
+                        </b>
+                        &mdash; {post.timestamp}
+                        <br />
+                        {post.text}
+                    </p>
+                );
+            })}
+        </>
     );
 }
