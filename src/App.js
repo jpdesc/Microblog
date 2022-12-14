6e6346871064d83728343b9e53d7327dfb516aed
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-14 13:47:56 -0800

    Add user page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
 import LoginPage from './pages/LoginPage';
+import UserPage from './pages/LoginPage';
 
 export default function App()
 {
@@ -15,6 +16,7 @@ export default function App()
 
                     <Route path="/" element={<FeedPage />} />
                     <Route path="/explore" element={<ExplorePage />} />
+                    <Route path="/user/:username" element={<UserPage />}/>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
