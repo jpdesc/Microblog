0030800d811dd1a4326e5cc0f3126d53cc88fde3
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-20 14:43:24 -0800

    Add Post component and edit Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import Post from './Post';
 
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Posts() {
     const [posts, setPosts] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(BASE_API_URL + '/api/feed');
+            if (response.ok) {
+                const results = await response.json();
+
+                setPosts(results.data);
+            }
+            else {
+                setPosts(null);
+            }
+        })();
+    }, []);
 
     return (
         <>
@@ -11,7 +27,17 @@ export default function Posts() {
                 <Spinner animation="border" />
                 :
                 <>
-                    ...
+                    {posts === null ?
+                        <p>Could not retrieve blog posts.</p>
+                        :
+                        <>
+                            {posts.length === 0 ?
+                                <p>There are no blog posts.</p>
+                                :
+                                posts.map(post => <Post key={post.id} post={post} />)
+                            }
+                        </>
+                    }
                 </>
             }
         </>
